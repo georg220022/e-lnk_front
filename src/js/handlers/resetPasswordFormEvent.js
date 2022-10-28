d2bf00bac390a3143461a9d24df65d53f9ced106
Gleb28 <iamgleb.g@gmail.com>
2022-10-28 14:40:42 +0300

    fix: bug with no back button after submitting forgot email form

--- src/js/handlers/resetPasswordFormEvent.js
+++ src/js/handlers/resetPasswordFormEvent.js
@@ -5,8 +5,8 @@ function resetPasswordFormEvent (event) {
 	}
 
 	if (event.target.closest('.back-button')) {
-		event.target.closest('form').style.display = 'none';
-		event.target.closest('form').previousElementSibling.style.display = 'block';
+		event.target.closest('.reset-block').style.display = 'none';
+		event.target.closest('.reset-block').previousElementSibling.style.display = 'block';
 	}
 }
 
--- src/js/templates/loginModalTemplate.js
+++ src/js/templates/loginModalTemplate.js
@@ -18,15 +18,17 @@ const loginModalTemplate = () => {
 						<button class="link-button forget-password-button" type="button">Забыли пароль?</button>
 						<button class="form__button form__button-login form__button--submit button--main" id="login-submitBtn" type="submit" name="submit">Войти</button>
 					</form>
-					<form class="form" id="reset-password-form" action="#" method="post" autocomplete="off" style="display: none">
+					<div class="reset-block" style="display: none">
 						<button class="link-button back-button" type="button">Назад</button>
-						<p class="reset-form-text">Введите почту, на которую зарегистрирован ваш аккаунт. На нее придет ссылка для сброса пароля.</p>
-						<label class="form__label form__label-reset-password--email" for="reset-password-email">
-							<input class="form__input form__input--reset-password-email" id="reset-password-email" type="email" name="email" placeholder="Введите e-mail" required>
-							<span class="error-label"></span>
-						</label>
-						<button class="form__button form__button--reset-password form__button--submit button--main" id="reset-password-form-submitBtn" type="submit" name="submit">Отправить</button>
-					</form>
+						<form class="form" id="reset-password-form" action="#" method="post" autocomplete="off" >
+							<p class="reset-form-text">Введите почту, на которую зарегистрирован ваш аккаунт. На нее придет ссылка для сброса пароля.</p>
+							<label class="form__label form__label-reset-password--email" for="reset-password-email">
+								<input class="form__input form__input--reset-password-email" id="reset-password-email" type="email" name="email" placeholder="Введите e-mail" required>
+								<span class="error-label"></span>
+							</label>
+							<button class="form__button form__button--reset-password form__button--submit button--main" id="reset-password-form-submitBtn" type="submit" name="submit">Отправить</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
